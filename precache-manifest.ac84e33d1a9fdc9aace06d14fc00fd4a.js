039a3c57c711e59e218b7c21f755257fb6127d45
Manasi <pateljinalben84@gmail.com>
2024-08-06 17:30:28 +0530

    Updates

---
+++ precache-manifest.ac84e33d1a9fdc9aace06d14fc00fd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6b1acddecca678a8d2f2c4a8236b2b",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "4338415093aa23f20b50",
+    "url": "/TextUtils-React/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "d26639b8c84f20273f60",
+    "url": "/TextUtils-React/static/js/2.e73d68c6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.e73d68c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0129b91c231a304bb30",
+    "url": "/TextUtils-React/static/js/3.6b0dde79.chunk.js"
+  },
+  {
+    "revision": "4338415093aa23f20b50",
+    "url": "/TextUtils-React/static/js/main.467be802.chunk.js"
+  },
+  {
+    "revision": "30f589f28d04f662d407",
+    "url": "/TextUtils-React/static/js/runtime-main.d3d7585c.js"
+  }
+]);
